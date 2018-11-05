52b2c23c2b16a52a2df029fe7b75a6191990440b
Marcelino <marcelino.malmeidan@gmail.com>
2018-11-05 15:57:47 -0600

    minor modification in python file

--- scripts/waypointGen.py
+++ scripts/waypointGen.py
@@ -19,11 +19,11 @@ alt = 3.5
 for z in range(2,3,1):
 	alt = 2.5 + z
 	print 'new z'
-	for i in range(0,1,1):
+	for i in range(0,2,1):
 		r = i + 6 
 		print 'new r'
-		for theta in range(270, 630, 5):
+		for theta in range(-90, 270, 15):
 			x = r*math.cos(math.radians(theta)) + x0
 			y = r*math.sin(math.radians(theta)) + y0
-			psi = theta - 270
+			psi = theta + 90
 			txt_outfile.write(str(x) + " " + str(y) + " " + str(alt) + " " + str(psi) + "\n")
